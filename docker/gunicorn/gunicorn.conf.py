35e0c91e1a3004f4150bd374e4897fb706dbae45
joce <trajkov.jordan18@gmail.com>
2026-03-31 23:21:34 +0200

    Reduce Gunicorn memory usage for Render free tier

--- docker/gunicorn/gunicorn.conf.py
+++ docker/gunicorn/gunicorn.conf.py
@@ -2,7 +2,9 @@ import multiprocessing
 import os
 
 bind = f"0.0.0.0:{os.getenv('PORT', '10000')}"
-workers = int(os.getenv("GUNICORN_WORKERS", multiprocessing.cpu_count() * 2 + 1))
+workers = int(os.getenv("GUNICORN_WORKERS", "1"))
+threads = int(os.getenv("GUNICORN_THREADS", "2"))
+worker_class = os.getenv("GUNICORN_WORKER_CLASS", "gthread")
 timeout = 120
 graceful_timeout = 30
 keepalive = 5
